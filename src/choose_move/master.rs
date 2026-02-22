671aa50ea7d024d6e9e150ba32e95ec0808b87d7
Folke Cederholm <folkecederholm@gmail.com>
2026-02-22 19:25:19 +0100

    Add depth search. Currently does 2-3 depth without becoming hideously slow.

--- src/choose_move/master.rs
+++ src/choose_move/master.rs
@@ -1,10 +1,10 @@
-use std::f64;
+use std::f64::{self, INFINITY, NEG_INFINITY};
 
 use crate::types::defs::*;
 
 #[allow(clippy::needless_pass_by_value)]
 pub fn choose_move(board: &Board, moves: Vec<ChessMove>) -> ChessMove {
-    let scores = moves.iter().map(|&x| (x, eval_move(board, x)));
+    let scores = moves.iter().map(|&x| (x, eval_move(board, x, 2)));
     let Some(chosen) = (match board.get_colour_turn() {
         // Colour::White => scores.min_by_key(|x| x.1),
         // Colour::Black => scores.max_by_key(|x| x.1),
@@ -16,43 +16,71 @@ pub fn choose_move(board: &Board, moves: Vec<ChessMove>) -> ChessMove {
     chosen.0
 }
 
-fn eval_move(board: &Board, chess_move: ChessMove) -> f64 {
-    let piece = board.get_tile(chess_move.start()).get_piece().unwrap();
-    let material = (match piece.colour {
-        Colour::White => 1.0,
-        Colour::Black => -1.0,
-    }) * (match piece.piece_type {
-        PieceType::Pawn => 1.0,
-        PieceType::Bishop => 3.2,
-        PieceType::Knight => 2.8,
-        PieceType::Rook => 5.0,
-        PieceType::Queen => 9.0,
-        PieceType::King => -2.0,
-    });
-    // let position = 2.0 - Coord::ay('a', 4).unwrap().manhattan(chess_move.end()) as f64 / 4.0;
-    let manhattan = {
-        let e4 = Coord::ay('e', 4).unwrap();
-        e4.manhattan(chess_move.end()) as f64
-    };
-    let position = 2.0 - manhattan / 4.0;
-    // println!("move: {chess_move:?}");
-    // println!("position: {position}");
-    // println!("manhattan: {manhattan}");
-    let taken = match board.get_tile(chess_move.end()).get_piece() {
-        Some(piece) => match piece.piece_type {
+fn eval_move(board: &Board, chess_move: ChessMove, depth: usize) -> f64 {
+    if depth == 0 {
+        let piece = board.get_tile(chess_move.start()).get_piece().unwrap();
+        let material = (match piece.colour {
+            Colour::White => 1.0,
+            Colour::Black => -1.0,
+        }) * (match piece.piece_type {
             PieceType::Pawn => 1.0,
             PieceType::Bishop => 3.2,
             PieceType::Knight => 2.8,
             PieceType::Rook => 5.0,
             PieceType::Queen => 9.0,
-            PieceType::King => 999.9,
-        },
-        None => 0.0,
-    } * match board.get_colour_turn() {
-        Colour::White => 1.0,
-        Colour::Black => -1.0,
-    };
-    println!("piece: {piece:?}");
-    println!("material: {material}");
-    material * position + taken
+            PieceType::King => -2.0,
+        });
+        // let position = 2.0 - Coord::ay('a', 4).unwrap().manhattan(chess_move.end()) as f64 / 4.0;
+        let manhattan = {
+            let e4 = Coord::ay('e', 4).unwrap();
+            e4.manhattan(chess_move.end()) as f64
+        };
+        let position = 2.0 - manhattan / 4.0;
+        // println!("move: {chess_move:?}");
+        // println!("position: {position}");
+        // println!("manhattan: {manhattan}");
+        let taken = match board.get_tile(chess_move.end()).get_piece() {
+            Some(piece) => match piece.piece_type {
+                PieceType::Pawn => 1.0,
+                PieceType::Bishop => 3.2,
+                PieceType::Knight => 2.8,
+                PieceType::Rook => 5.0,
+                PieceType::Queen => 9.0,
+                PieceType::King => 999.9,
+            },
+            None => 0.0,
+        } * match board.get_colour_turn() {
+            Colour::White => 1.0,
+            Colour::Black => -1.0,
+        };
+        // println!("piece: {piece:?}");
+        // println!("material: {material}");
+        // println!("{chess_move:?}/{}", material * position + taken);
+        material * position + taken
+    } else {
+        // depth > 0
+        // All the moves you opponent could answer with
+        let state = {
+            let mut s = board.clone();
+            s.make_move(chess_move).unwrap();
+            s
+        };
+        let answers = state.find_all_moves();
+        let mut extreme = match state.get_colour_turn() {
+            Colour::White => NEG_INFINITY,
+            Colour::Black => INFINITY,
+        };
+        for answer in answers {
+            let score = eval_move(&state, answer, depth - 1);
+            if match state.get_colour_turn() {
+                Colour::White => score > extreme,
+                Colour::Black => score < extreme,
+            } {
+                // println!("from {extreme} to {score}");
+                extreme = score;
+                // println!("(new):{extreme}/{chess_move:?}");
+            }
+        }
+        extreme
+    }
 }
